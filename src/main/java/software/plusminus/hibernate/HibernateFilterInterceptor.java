2a7358ea298ee74cdf0a237a96d9f4d3de05afc5
Taras Shpek <taras.shpek@gmail.com>
2021-07-14 00:40:57 +0300

    improve hibernate interceptor

--- src/main/java/software/plusminus/hibernate/HibernateFilterInterceptor.java
+++ src/main/java/software/plusminus/hibernate/HibernateFilterInterceptor.java
@@ -3,6 +3,7 @@ package software.plusminus.hibernate;
 import org.hibernate.Filter;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -16,12 +17,15 @@ import javax.persistence.PersistenceContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("PMD.CloseResource")
+@ConditionalOnBean(HibernateFilter.class)
 @Component
 public class HibernateFilterInterceptor implements HandlerInterceptor, WebMvcConfigurer {
 
+    private  static final String ATTRIBUTE_NAME = "hibernateSession";
+    
     @PersistenceContext
     private EntityManager entityManager;
-
     @Autowired
     private List<HibernateFilter> filters;
 
@@ -37,16 +41,17 @@ public class HibernateFilterInterceptor implements HandlerInterceptor, WebMvcCon
             Filter filter = session.enableFilter(f.filterName());
             f.parameters().forEach(filter::setParameter);
         });
-        request.setAttribute("hibernateSession", session);
+        request.setAttribute(ATTRIBUTE_NAME, session);
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
                            Object handler, ModelAndView modelAndView) {
-        Session session = (Session) request.getAttribute("hibernateSession");
+        Session session = (Session) request.getAttribute(ATTRIBUTE_NAME);
         if (session != null) {
             filters.forEach(f -> session.disableFilter(f.filterName()));
+            request.removeAttribute(ATTRIBUTE_NAME);
         }
     }
 }
