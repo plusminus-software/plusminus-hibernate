95a6ccd718d0590f4cb9167cc04db2e937206c29
Taras Shpek <taras.shpek@gmail.com>
2021-10-30 19:55:53 +0300

    add autoconfig class

---
+++ src/main/java/software/plusminus/hibernate/HibernateAutoconfig.java
@@ -0,0 +1,9 @@
+package software.plusminus.hibernate;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("software.plusminus.hibernate")
+public class HibernateAutoconfig {
+}
